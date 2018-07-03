290c3e1d474bed659e88561f283ad2a5c4451c18
Lukas Bühler <lukasmc6@gmail.com>
2018-07-03 20:16:02 +0200

    more multilanguage stuff

--- src/scripts/multi_lang.ts
+++ src/scripts/multi_lang.ts
@@ -42,6 +42,14 @@ export function makeMultilangResource(
     return resource;
 }
 
+let my18nextVars = 
+{
+    "generalLanguageFileName": "general",
+    "classesGroupName": "classes",
+    "insertHtmlCommandClass": "i18n-insert-as-html",
+    "classesSelectorClassPrefix": "i18n-c-",
+}
+
 
 const instance = i18n
     .use(XHR)
@@ -68,6 +76,10 @@ function addResouces(lang: string, namespace: string, data: Object)
 function updateContent()
 {
     // Update all the elements with the accroding text
+    let params = 
+    {
+        age: 20 // TODO
+    }
 
     // Get language
     let lng = "en" // as of yet hardcoded
@@ -84,7 +96,7 @@ function updateContent()
             {
                 for (let elem in data[group])
                 {
-                    $(`#${group} .${elem}`).text(i18n.t(`${page}:${group}.${elem}`))
+                    insertI18nextPhrase(page, group, params, [elem], true);
                 }
             }
         });
@@ -94,14 +106,14 @@ function updateContent()
             // Get ids
             for (let group in data)
             {
-                if(group === "classes")
+                if(group === my18nextVars.classesGroupName)
                 {
-                    // Don't match ids only classes.
+                    // Don't match groups.
                     for (let elem in data[group])
                     {
                         for (let elem2 in data[group][elem])
                         {
-                            $(`.i18n-${elem}-${elem2}`).text(i18n.t(`general:${group}.${elem}.${elem2}`))
+                            insertI18nextPhrase(my18nextVars.generalLanguageFileName, group, params, [elem, elem2], false)
                         }
                     }
                 }
@@ -109,13 +121,11 @@ function updateContent()
                 {
                     for (let elem in data[group])
                     {
-                        $(`#${group} .${elem}`).text(i18n.t(`general:${group}.${elem}`))
+                        insertI18nextPhrase(my18nextVars.generalLanguageFileName, group, params, [elem], true)
                     }
                 }
             }
         });
-
-        //$("#welcome .intro").text(i18n.t("index:welcome.intro"))
     }
 }
 
@@ -124,6 +134,29 @@ function updateCardContent(language)
 {
     //Update the cards with the correct language
     
+}
+
+function insertI18nextPhrase(file: String, group: String, params: Object, elems: String[], isMatchingGroup: boolean)
+{
+    let element: JQuery<HTMLElement>
+    if(isMatchingGroup)
+    {
+        element = $(`#${group} .${elems.join(".")}`);
+    }
+    else
+    {
+        element = $(`.${my18nextVars.classesSelectorClassPrefix}${elems.join("-")}`)
+        console.log(`.${my18nextVars.classesSelectorClassPrefix}${elems.join("-")}`);
+    }
+
+    if(element.hasClass(my18nextVars.insertHtmlCommandClass))
+    {
+        element.html(i18n.t(`${file}:${group}.${elems.join(".")}`, params))
+    }
+    else
+    {
+        element.text(i18n.t(`${file}:${group}.${elems.join(".")}`, params))
+    }
 }
 
 function changeLng(lng)
