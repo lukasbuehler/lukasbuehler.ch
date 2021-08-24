e48a3a4ef44c7dc746269f591337db8b102189ea
Lukas Bühler <mail@lukasbuehler.ch>
2021-08-24 11:07:53 +0200

    tried around with theming and created component files

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,14 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+
+//import light from "./vuetify/themes/light_theme";
+//import dark from "./vuetify/themes/dark_theme";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    dark: true,
+    //themes: { dark },
+  },
 });
