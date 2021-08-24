74e7b92a1236e09e5b3a3ef14ac9e0c7871bbdab
Lukas Bühler <mail@lukasbuehler.ch>
2021-08-24 09:44:20 +0200

    vuetify installation workaround

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// TODO remove
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");
+
+createApp(App).mount("#app");
