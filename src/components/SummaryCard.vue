8d1caa78a4d388e5e5ef496dd524ef1b4caed780
Lukas Bühler <mail@lukasbuehler.ch>
2022-10-03 09:25:53 +0200

    Worked on studies page

--- src/components/SummaryCard.vue
+++ src/components/SummaryCard.vue
@@ -2,26 +2,41 @@
   <v-card class="mx-auto text-left" max-width="344" outlined>
     <v-list-item three-line>
       <v-list-item-content>
-        <div v-if="department" class="text-overline mb-4">
+        <div v-if="department" class="text-overline mb-3">
           {{ department }}
         </div>
-        <v-list-item-title class="text-h5 mb-1 text-wrap">
+        <v-list-item-title class="text-h5 mb-3 text-wrap">
           {{ name }}
         </v-list-item-title>
-        <v-list-item-subtitle v-if="semester || profName" 
+        <v-list-item-subtitle v-if="semester || profName"
           >{{ semester }}: {{ profName }}</v-list-item-subtitle
         >
-        <v-list-item-subtitle v-if="authors" class="mt-2"
-          >{{authors}}</v-list-item-subtitle
-        >
+        <v-list-item-subtitle v-if="authors" class="mt-2">{{
+          authors
+        }}</v-list-item-subtitle>
+        <div v-if="isUnfinished" class="text-danger mt-4 mb-2">
+          {{ isUnfinished ? "Not finished" : "" }}
+        </div>
       </v-list-item-content>
     </v-list-item>
 
     <v-card-actions>
-      <v-btn v-if="pdfLink" outlined rounded text v-on:click="openLink(pdfLink)">
+      <v-btn
+        v-if="pdfLink"
+        outlined
+        rounded
+        text
+        v-on:click="openLink(pdfLink)"
+      >
         PDF
       </v-btn>
-      <v-btn v-if="repoLink" outlined rounded text v-on:click="openLink(repoLink)">
+      <v-btn
+        v-if="repoLink"
+        outlined
+        rounded
+        text
+        v-on:click="openLink(repoLink)"
+      >
         SOURCE CODE
       </v-btn>
     </v-card-actions>
@@ -31,7 +46,16 @@
 <script>
 export default {
   name: "SummaryCard",
-  props: ["name", "semester", "department", "profName", "pdfLink", "repoLink", "authors"],
+  props: [
+    "name",
+    "semester",
+    "department",
+    "profName",
+    "pdfLink",
+    "repoLink",
+    "authors",
+    "isUnfinished",
+  ],
 
   components: {},
 
@@ -41,9 +65,13 @@ export default {
   methods: {
     openLink: function(link_) {
       window.open(link_);
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style></style>
+<style>
+.text-danger {
+  color: rgba(255, 0, 0, 0.9);
+}
+</style>
--- src/views/Studies.vue
+++ src/views/Studies.vue
@@ -3,10 +3,16 @@
     <v-container class="text-center" style="padding-top:50px;">
       <v-row class="text-center">
         <v-col cols="12">
-          <h1 class="display-2">Summaries</h1>
-          <p>This sections contains summaries I've created during my studies.
-            I studied Computational Science and Engineering at ETH Zürich.
+          <h1 class="display-2 mb-3">Summaries</h1>
+          <p>
+            This sections contains summaries I've created during my Bachelor
+            studies of Computational Science and Engineering at ETH Zürich.
           </p>
+          <h4 class="text-danger">
+            All the following summaries are provided "as is" and without any
+            warranty. It is possible that there are mistakes, use at your own
+            risk.
+          </h4>
         </v-col>
       </v-row>
       <v-row>
@@ -29,6 +35,7 @@
               repoLink="https://gitlab.ethz.ch/buehlluk/phys-chem-3-summary-fs22/-/tree/master/"
               pdfLink="https://gitlab.ethz.ch/buehlluk/phys-chem-3-summary-fs22/-/raw/master/PCIII.pdf"
               authors="Adel Gavranović, Lukas Bühler"
+              isUnfinished="true"
             />
             <SummaryCard
               name="Numerical Methods for Partial Differential Equations"
@@ -37,12 +44,15 @@
               semester="FS 2022"
               repoLink="https://gitlab.ethz.ch/lukasbuehler_summaries/numpde_summary_fs22"
               pdfLink="https://gitlab.ethz.ch/lukasbuehler_summaries/numpde_summary_fs22/-/raw/master/numpde_summary_fs22.pdf"
+              isUnfinished="true"
             />
-            <SummaryCard 
+            <SummaryCard
               name="Statistische Physik"
               department="RW"
               semester="FS 2022"
-              profName="S. Riniker"
+              profName="S. Riniker, P. H. Hünenberger"
+              repoLink="https://gitlab.ethz.ch/lukasbuehler_summaries/statistical-physics-and-computer-simulation-for-cse-summary"
+              pdfLink="https://gitlab.ethz.ch/lukasbuehler_summaries/statistical-physics-and-computer-simulation-for-cse-summary/-/raw/main/SPCS_Summary.pdf"
             />
           </div>
         </v-col>
@@ -126,5 +136,8 @@ export default {
   gap: 10px;
   justify-items: stretch;
   align-items: stretch;
+}
+.text-danger {
+  color: rgba(255, 0, 0, 0.9);
 }
 </style>
